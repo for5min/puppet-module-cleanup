296c4cb5c5d31c67f1f9f52f8436ec704d162685
Alex Tu <alex.lei.tu@gmail.com>
2013-10-24 05:20:12 +0000

    add

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -3,14 +3,9 @@ require 'spec_helper'
 describe 'cleanup' do
 
   let(:hiera_data) {{
-    'cleanup::target' => { 'tmp' =>{
-    'age'     => '21d',
-    'path'    => '/tmp',
-    'matches' => '[ "*.xml", "*.txt" ]',
-    'recurse' => 'true',
-   } }
+    'target' => 'tmp'
   }}
-  it { should include_class('cleanup').with_tidy('tmp')
+  it { should include_class('cleanup').with_target('tmp')
   }
 end
 
