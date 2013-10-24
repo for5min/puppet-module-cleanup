fb9c022aa91a1acd148ec5be1b00a2098f674b0e
Alex Tu <alex.lei.tu@gmail.com>
2013-10-24 05:36:41 +0000

    add

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -5,7 +5,7 @@ describe 'cleanup' do
   let(:hiera_data) {{
     'cleanup::target' => 'tmp'
   }}
-  it { should contain_class('cleanup').create_resource('cleanup::target')
+  it { should contain_class('cleanup').create_resources('cleanup::target')
   }
 end
 
